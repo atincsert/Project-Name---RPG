edeb4d770bf97ce8b7e72a75fb7b1d1f47072ef0
Atınç Sert <ryu_atinc@hotmail.com>
2022-03-25 19:03:38 +0300

    Added namespaces to scripts to layer the dependencies

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField] Transform target;
-    private void Update()
-    {               
-        if (Input.GetMouseButtonDown(0))
-        {
-            MoveToCursor();
-        }
-    }
-
-    private void MoveToCursor()
+    public class Mover : MonoBehaviour
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-
-        if (hasHit == true)
+        private void Update()
         {
-            GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            UpdateAnimator();
         }
-        
-        
-        
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().SetDestination(destination);
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
