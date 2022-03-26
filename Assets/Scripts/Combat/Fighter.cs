d76effb268887216fb65db36f2e9fc0a4a65fc6f
Atınç Sert <ryu_atinc@hotmail.com>
2022-03-26 19:15:26 +0300

    Introduced interfaces and removed dependicies from mover to combat

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+
+        public Transform target;
+        private void Update()
+        {
+            if (target == null) return;
+            
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+                Debug.Log("ORAORAORAORAORAORAORA");
+            }            
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;            
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
