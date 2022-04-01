5f72e10eb43f635bef879e97a5aa521725a21b1e
Atınç Sert <ryu_atinc@hotmail.com>
2022-04-01 15:52:16 +0300

    Added some lines to AIController to make enemies chase player when out of range be suspicious for a while and go back to their post

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -3,41 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
 
         private PlayerController player;
         private Fighter fighter;
         private Health health;
+        private Vector3 guardPosition;
+        private Mover mover;
+        private float timeSinceLastSaw = Mathf.Infinity;
         private void Awake()
         {
             health = GetComponent<Health>();
             player = FindObjectOfType<PlayerController>();
             fighter = GetComponent<Fighter>();
+            mover = GetComponent<Mover>();
+
+            guardPosition = transform.position;
         }
         private void Update()
-        {
+        {            
             if (health.IsDead()) return;
-           
+
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player.gameObject))
             {
-                fighter.Attack(player.gameObject);
+                timeSinceLastSaw = 0f;
+                AttackState();
+            }
+            else if (timeSinceLastSaw < suspicionTime)
+            {
+                SuspicionState();
             }
             else
             {
-                fighter.Cancel();
+                GuardState();
             }
 
+            timeSinceLastSaw += Time.deltaTime;
+        }
+
+        private void GuardState()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionState()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackState()
+        {
+            fighter.Attack(player.gameObject);
         }
 
         private bool InAttackRangeOfPlayer()
         {            
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);            
         }
     }
 }
