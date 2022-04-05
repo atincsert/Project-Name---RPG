51817d54cb8e7a7048d94ba7d89c671f4a6580b4
Atınç Sert <ryu_atinc@hotmail.com>
2022-04-05 20:32:22 +0300

    Made a timeline consisting of enemy reveals with multiple camera

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -14,6 +14,7 @@ namespace RPG.Control
         [SerializeField] PatrolPath patrolPath;
         [SerializeField] float waypointTolerance = 1f;
         [SerializeField] float waypointDwellTime = 3f;
+        [Range(0, 1)] [SerializeField] float patrolSpeedFraction = 0.2f;
 
         private PlayerController player;
         private Fighter fighter;
@@ -69,7 +70,7 @@ namespace RPG.Control
             }
             if (timeSinceArrivedAtWaypoint > waypointDwellTime)
             {
-                mover.StartMoveAction(nextPosition);
+                mover.StartMoveAction(nextPosition, patrolSpeedFraction);
             }
         }
 
