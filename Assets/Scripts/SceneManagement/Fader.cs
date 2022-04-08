23b101240c2034f22ee247346dd5703068004414
Atınç Sert <ryu_atinc@hotmail.com>
2022-04-08 18:22:08 +0300

    Added a fader to fade out and in black while loading the next scene also set portals so that each portal has different reference in the next scene

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {        
+        private CanvasGroup canvasGroup;
+
+        private void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) // alpha is not 1
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+                // update alpha
+            }         
+        }
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) // alpha is not 0
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+                // update alpha
+            }
+        }
+    }
+}
--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,5 @@
+using UnityEngine.AI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +9,18 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 2f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = 0.5f;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
@@ -18,10 +31,44 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load is not set");
+                yield break;
+            }
+
+            Fader fader = FindObjectOfType<Fader>();
             DontDestroyOnLoad(gameObject);
+            yield return fader.FadeOut(fadeOutTime);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            Debug.Log("Scene Loaded");
+            StartCoroutine(fader.FadeIn(fadeInTime));
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
