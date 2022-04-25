671e202a2c59efe2cc90571de79443bb93563624
Atınç Sert <ryu_atinc@hotmail.com>
2022-04-25 21:01:41 +0300

    Made a fireball as a prefab variant for a magic projectile

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,14 +9,23 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float arrowSpeed = 2f;
+        [SerializeField] bool isHoming = true;
 
         private Health target = null;
-        private float damage; 
+        private float damage;
 
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
         private void Update()
         {
             if (target == null) return;
-            transform.LookAt(GetAimLocation());
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+
             transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
         }
 
@@ -40,7 +49,7 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != target) return;
-
+            if (target.IsDead()) return;
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
