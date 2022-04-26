cf9cc6de20bd7c125cb171744ca43a718f0602f8
Atınç Sert <ryu_atinc@hotmail.com>
2022-04-26 17:11:10 +0300

    Made impact effects for all projectiles and changed fireball's also fixed some bugs

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -8,11 +8,16 @@ namespace RPG.Combat
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] float arrowSpeed = 2f;
+        [SerializeField] float projectileSpeed = 30f;
         [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10f;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        [SerializeField] float lifeAfterImpact = 2f;
 
         private Health target = null;
         private float damage;
+        
 
         private void Start()
         {
@@ -26,13 +31,15 @@ namespace RPG.Combat
                 transform.LookAt(GetAimLocation());
             }
 
-            transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
         }
 
         public void SetTarget(Health target, float damage)
         {
             this.target = target;
             this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
         }
 
         private Vector3 GetAimLocation()
@@ -51,7 +58,19 @@ namespace RPG.Combat
             if (other.GetComponent<Health>() != target) return;
             if (target.IsDead()) return;
             target.TakeDamage(damage);
-            Destroy(gameObject);
-        }
+
+            projectileSpeed = 0f;
+
+            if (hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+            foreach (GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeAfterImpact);
+        }      
     }
 }
